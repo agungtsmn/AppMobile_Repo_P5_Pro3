94c81c703e5c1735693ae68124088e9af9f36cc2
agungtsmn <0812agung@gmail.com>
2022-04-09 13:01:44 +0700

    Initial Commit - Update

--- app/src/main/java/id/ac/polbeng/amandaagungpermata/p5pro3/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/amandaagungpermata/p5pro3/MainActivity.kt
@@ -1,11 +1,36 @@
 package id.ac.polbeng.amandaagungpermata.p5pro3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
+
+    val Log = Logger.getLogger(MainActivity::class.java.name)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.info("onCreate")
+        btnToSecondActivity.setOnClickListener {
+            val mIntent = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(mIntent)
+        }
     }
+    override fun onPause() {
+        super.onPause()
+        Log.info("onPause")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.info("onRestart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.info("onResume")
+    }
+
 }
---
+++ app/src/main/java/id/ac/polbeng/amandaagungpermata/p5pro3/SecondActivity.kt
@@ -0,0 +1,29 @@
+package id.ac.polbeng.amandaagungpermata.p5pro3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+import java.util.logging.Logger
+
+class SecondActivity : AppCompatActivity() {
+
+    val Log = Logger.getLogger(MainActivity::class.java.name)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        Log.info("onCreate")
+        btnExitActivity.setOnClickListener {
+            finish()
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.info("onStart")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.info("onStop")
+    }
+}
