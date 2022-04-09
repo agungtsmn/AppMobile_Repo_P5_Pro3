6609d5e5d35fd247d725220fbcc542f7c4c18f1e
agungtsmn <0812agung@gmail.com>
2022-04-09 12:51:39 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polbeng/amandaagungpermata/p5pro3/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.polbeng.amandaagungpermata.p5pro3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
